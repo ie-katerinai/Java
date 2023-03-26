66b245939acee34cf5b79081bbe8426a8ef8f89f
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-03-26 20:39:30 +0300

    Add task_3

--- HomeWork/HW_2/Task_3.java
+++ HomeWork/HW_2/Task_3.java
@@ -10,39 +10,23 @@
 Студент Краснов получил 5 по предмету Физика. */
 package HomeWork.HW_2;
 
-import java.io.*;
 import java.util.Scanner;
-import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Task_3 {
     public static void main(String[] args) {
         String file = "/Users/evahome/Documents/Java/HomeWork/HW_2/students_list.json";
         String json = readFile(file);
-        formatString(json);
-        System.out.println(json);
-        HashMap<String, String> dictionary = new HashMap();
-        dictionary = createDictionaryFromJson(json);
-        System.out.println(dictionary.values());
-        // String textReplace = json.replaceAll("\"", "");
-        // String[] textArray = textReplace.split(",");
-        // StringBuilder requestText = new StringBuilder(req);
-        //
-        // }
-        // }
-        // requestText.replace(requestText.lastIndexOf(" AND"), requestText.length(),
-        // "");
-        // System.out.println(requestText);
+        String newJson = formatString(json);
+        String[] stringArray = createArrayFromJson(newJson);
+        makeStringFromArray(stringArray);
     }
 
-    public static void formatString(String formatedText) {
-        formatedText.replaceAll("[^0-9a-zA-Z:,]", "");
+    public static String formatString(String formatedText) {
+        String text = formatedText.replaceAll("[^0-9a-zA-Zа-яА-Я:,]", "");
+        return text;
     }
 
     public static String readFile(String fileName) {
@@ -51,32 +35,30 @@ public class Task_3 {
             Scanner scanner = new Scanner(Paths.get(fileName), StandardCharsets.UTF_8.name());
             fileString = scanner.useDelimiter("\\A").next();
             scanner.close();
-            // return fileString;
         } catch (IOException e) {
             System.out.println(e);
         }
         return fileString;
     }
 
-    public static HashMap createDictionaryFromJson(String text) {
-        HashMap<String, String> dict = new HashMap<>();
+    public static String[] createArrayFromJson(String text) {
         String[] stringArray = text.toString().split(",");
+        String[] array = new String[stringArray.length];
+        int i = 0;
         for (String word : stringArray) {
             int position = word.indexOf(":");
-            String key = word.substring(0, position);
             String value = word.substring(position + 1);
-            dict.put(key, value);
+            array[i] = value;
+            i++;
         }
-        return dict;
+        return array;
     }
 
     public static void makeStringFromArray(String[] stringArray) {
         StringBuilder file = new StringBuilder();
-        for (String word : stringArray) {
-            int position = word.indexOf(":");
-            file.append(word.substring(0,
-                    position)).append("=\'").append(word.substring(position + 1))
-                    .append("\' AND");
+        for (int i = 0; i < stringArray.length; i = i + 3) {
+            file.append("Студент ").append(stringArray[i]).append(" получил ").append(stringArray[i+1]).append(" по предмету ").append(stringArray[i+2]).append(". ");
         }
+        System.out.println(file);
     }
 }
