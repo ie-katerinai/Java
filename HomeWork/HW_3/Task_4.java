8d61e765994c5eb71f1f3e82cf2fc34d08669c9c
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-04-01 14:52:00 +0300

    Add HW 3 Task 4

---
+++ HomeWork/HW_3/Task_4.java
@@ -0,0 +1,10 @@
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
+package HomeWork.HW_3;
+
+public class Task_4 {
+    // public static void main(String[] args) {
+    //     int array[] = { 9, 1, 3, 15, 4, 14, 7, 6, 8, 11 };
+    //     ArrayList<Integer> list = createListFromArrayList(array);
+    //     removeEvenElements(list);
+    // }
+}
