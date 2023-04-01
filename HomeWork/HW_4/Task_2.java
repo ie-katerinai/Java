368c0a7300121504a0fc95e890762f27697a608c
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-04-01 15:25:04 +0300

    Add HW 4 Task 2

--- HomeWork/HW_4/Task_2.java
+++ HomeWork/HW_4/Task_2.java
@@ -4,6 +4,37 @@
 // • first() — возвращает первый элемент из очереди, не удаляя.
 package HomeWork.HW_4;
 
+import java.util.LinkedList;
+
 public class Task_2 {
-    
+    public static void main(String[] args) {
+        LinkedList<String> queueList = new LinkedList<>();
+        enqueue(queueList, "First");
+        enqueue(queueList, "Second");
+        enqueue(queueList, "Third");
+        enqueue(queueList, "Fourth");
+
+        System.out.println(queueList);
+        String firstElement = dequeue(queueList);
+        System.out.println(firstElement);
+        System.out.println(queueList);
+        firstElement = first(queueList);
+        System.out.println(firstElement);
+        System.out.println(queueList);
+    }
+
+    public static void enqueue(LinkedList<String> list, String element) {
+        list.add(element);
+    }
+
+    public static String dequeue(LinkedList<String> list) {
+        String element = list.getFirst();
+        list.removeFirst();
+        return element;
+    }
+
+    public static String first(LinkedList<String> list) {
+        String element = list.getFirst();
+        return element;
+    }
 }
