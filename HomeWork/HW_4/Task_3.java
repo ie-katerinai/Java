135de2afe6b07f6dcd4f46f72954f9297a16ac7b
ie-katerinai <117760910+ie-katerinai@users.noreply.github.com>
2023-04-01 15:56:15 +0300

    Add HW 4 Task 3

--- HomeWork/HW_4/Task_3.java
+++ HomeWork/HW_4/Task_3.java
@@ -1,6 +1,44 @@
 // В калькулятор добавьте возможность отменить последнюю операцию.
 package HomeWork.HW_4;
 
+import java.util.Scanner;
+
 public class Task_3 {
-    
+    public static void main(String[] args) {
+
+        String flag = "1";
+        while (flag != "x") {
+
+            System.out.println("Введите первое число: ");
+            Scanner scanner = new Scanner(System.in);
+            int numb_1 = Integer.parseInt(scanner.nextLine());
+            System.out.println("Введите операцию: ");
+            String operator = scanner.nextLine();
+            System.out.println("Введите второе число: ");
+            int numb_2 = Integer.parseInt(scanner.nextLine());
+            int result = 0;
+            switch (operator) {
+                case "+":
+                    result = numb_1 + numb_2;
+                    break;
+                case "-":
+                    result = numb_1 - numb_2;
+                    break;
+                case "*":
+                    result = numb_1 * numb_2;
+                    break;
+                case "/":
+                    result = numb_1 / numb_2;
+                    break;
+                default:
+                    System.out.println("Не возвозможно совершить операцию.");
+                    break;
+            }
+            System.out.println("Результат операции: " + result);
+            System.out.println("Нажмите x для выхода из калькулятора, или любую клавишу для продолжения");
+            flag = scanner.nextLine();
+            System.out.println(flag);
+
+        }
+    }
 }
